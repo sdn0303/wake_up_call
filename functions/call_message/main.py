9c18babab7cd2bc60346b1d5b1a881d603a748a1
sdn0303 <sdn0303.guitar@gmail.com>
2020-01-21 22:41:58 +0900

    change cloud run to functions.

--- functions/call_message/main.py
+++ functions/call_message/main.py
@@ -3,6 +3,10 @@ from twilio.twiml.voice_response import VoiceResponse
 
 
 def message(request):
+    print(request.get_json())
+
     resp = VoiceResponse()
-    resp.say("朝だ！起きろー！！!", language="ja-JP", voice="Polly.Mizuki")
+    resp.say("朝だ！起きろおおおおおお！", language="ja-JP", voice="Polly.Mizuki")
     print(str(resp))
+
+    return str(resp)
